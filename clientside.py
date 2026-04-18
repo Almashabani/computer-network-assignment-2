e9122e9bbe0d6865714dafcf95da106350751ef6
gjelbrimmorina <gjelbrim.morina@student.uni-pr.edu>
2026-04-18 20:36:56 +0200

    Update clientside.py
    Ndryshimi i hostanme dhe portnumber

--- clientside.py
+++ clientside.py
@@ -4,8 +4,8 @@ import socket
 mySocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0, fileno=None)
 print("Socketi eshte gati i gjeneruar")
 
-hostname = "172.16.100.239"
-portno = 9997
+hostname = " "
+portno = 0000
 
 mySocket.connect((hostname, portno))
 print("Lidhja eshte arritur me serverin")
