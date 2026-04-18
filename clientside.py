2af1839a154381c7729ea988a43560b770cd4643
gjelbrimmorina <gjelbrim.morina@student.uni-pr.edu>
2026-04-18 15:15:26 +0200

    Update clientside.py
    Complete TCP client implementation

--- clientside.py
+++ clientside.py
@@ -9,3 +9,34 @@ portno = 9997
 
 mySocket.connect((hostname, portno))
 print("Lidhja eshte arritur me serverin")
+
+# Jep emrin e klientit (John do jete admin)
+client_name = input("Shkruaje emrin e klientit: ")
+mySocket.send(client_name.encode())
+
+msg_in = mySocket.recv(2048).decode()
+print("\nPergjigja nga serveri:")
+print(msg_in)
+
+while True:
+    print("\nShembuj komandash:")
+    print("LIST")
+    print("READ test.txt")
+    print("WRITE test.txt Pershendetje nga klienti")
+    print("EXEC program.py")
+    print("EXIT")
+
+    msg = input("\nShkruaj komanden qe deshiron ta dergosh ne server: ")
+    print("Mesazhi i derguar ne server:", msg)
+
+    mySocket.send(msg.encode())
+
+    response = mySocket.recv(4096).decode()
+    print("\nPergjigja nga serveri:")
+    print(response)
+
+    if msg.strip().upper() == "EXIT":
+        break
+
+mySocket.close()
+print("Lidhja eshte arritur me sukses dhe komunikimi eshte bere me serverin")
